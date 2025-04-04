1455772fabaf3546d7564c1594fe3a47952c470e
flavioaugusto1 <f.augustosdn@gmail.com>
2025-04-03 23:07:05 -0300

    projeto

--- src/middlewares/veirfy-user-authorization.ts
+++ src/middlewares/veirfy-user-authorization.ts
@@ -1,14 +1,14 @@
-import { NextFunction, Request, Response } from 'express';
-import { AppError } from '../utils/AppError';
+import { NextFunction, Request, Response } from 'express'
+import { AppError } from '../utils/AppError'
 
-type UserRole = 'employee' | 'manager';
+type UserRole = 'employee' | 'manager'
 
-export function verifyUserAuthorization(role: UserRole[]){
-    return (request: Request, response: Response, next: NextFunction) => {
-        if(!request.user || !role.includes(request.user.role as UserRole)){
-            throw new AppError('Unauthorized', 401)
-        }
-
-        return next()
+export function verifyUserAuthorization(role: UserRole[]) {
+  return (request: Request, response: Response, next: NextFunction) => {
+    if (!request.user || !role.includes(request.user.role as UserRole)) {
+      throw new AppError('Unauthorized', 401)
     }
+
+    return next()
+  }
 }
